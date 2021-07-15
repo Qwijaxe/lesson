d4ffe85418d323b4f9850de537cb65265e7c7037
Qwijaxe <qwijaxe@gmail.com>
2021-07-15 16:34:06 +0300

    сделал циклы

--- index.js
+++ index.js
@@ -82,13 +82,11 @@ do {
   num = prompt("Введите число, большее 100?", 0);
 } while (num <= 100 && num); */
 
-let n = 10;
-let i;
-let j;
+/* let n = 10;
 
-outer: for (i = 2; i <= n; i++) {
-  for (j = 2; j < i; j++) {
+outer: for (let i = 2; i <= n; i++) {
+  for (let j = 2; j < i; j++) {
     if (i % j === 0) continue outer
   }
   console.log(i)
-}
+} */
