267080d81099a53c46e1fb22b87710ce0c983627
Qwijaxe <qwijaxe@gmail.com>
2021-07-15 16:32:27 +0300

    сделал циклы

--- index.js
+++ index.js
@@ -59,3 +59,36 @@ if (userName == 'Админ') {
   console.log( "Я вас не знаю" );
 }*/
 
+/* let i = 3;
+while (i) {
+  console.log( i );
+  i--;
+} */
+
+/* for (let i = 2; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log( i );
+  }
+} */
+
+/* let i = 0;
+while (i < 3) {
+  console.log( `number ${i}!` );
+  i++;
+} */
+
+/* let num;
+do {
+  num = prompt("Введите число, большее 100?", 0);
+} while (num <= 100 && num); */
+
+let n = 10;
+let i;
+let j;
+
+outer: for (i = 2; i <= n; i++) {
+  for (j = 2; j < i; j++) {
+    if (i % j === 0) continue outer
+  }
+  console.log(i)
+}
